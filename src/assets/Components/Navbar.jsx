06c09b40057b5a27452bf2be6b29c97fe5f0a659
PankajKDev <pankajk.dev0000@gmail.com>
2025-02-02 22:32:27 +0530

    updated icons and changes

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -4,9 +4,11 @@ const Navbar = () => {
   return (
     <div className="w-full sticky h-14 bg-black text-left flex items-center justify-between ">
       <NavLink to="/">
-        <h2 className="text-white  text-2xl ml-5 font-mono font-semibold text-pretty">
-           SJ<span className="text-4xl text-[#D3B0E0]  ml-2 font-bold">&hearts;</span>                
-        </h2>
+        <div className="text-white inline-flex  text-2xl ml-5 font-mono font-semibold text-pretty">
+           SJ
+            <img className="w-6" src="/heart.svg" alt="" />
+                        
+        </div>
         </NavLink>
       <NavLink to="/map">
       <img className="w-10 h-10 mr-10" src="/Images/NavCompass.png" alt="" />
